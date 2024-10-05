8aeb21e9d024c870a46ccac9bd177750a2788ee1
mamun <mamunopstel@gmail.com>
2024-10-05 17:23:38 +0600

    finished Infrastructure, Our Way of Thinking, Our Branches sections

--- js/globalcomponents.js
+++ js/globalcomponents.js
@@ -9,8 +9,42 @@ function toggleSideMenu() {
 sideMenuToggler.addEventListener("click", toggleSideMenu);
 sideMenuCloseBtn.addEventListener("click", toggleSideMenu);
 
-
-const navUl = document.getElementById("nav-ul")
+const navUl = document.getElementById("nav-ul");
 navUl.querySelector("a").addEventListener("click", toggleSideMenu);
 
 // Script for SIDE-MENUBAR section ends here
+
+// For Common component Hero Slides
+const slides = document.getElementsByClassName("slide");
+
+let currentIndex = 0;
+let currentSlide = slides[currentIndex];
+
+currentSlide.classList.add("current-slide");
+
+function nextSlide() {
+  currentSlide.classList.remove("current-slide");
+  if (currentIndex === slides.length - 1) {
+    currentIndex = 0;
+    currentSlide = slides[currentIndex];
+    currentSlide.classList.add("current-slide");
+    return;
+  }
+  currentIndex += 1;
+  currentSlide = slides[currentIndex];
+  currentSlide.classList.add("current-slide");
+}
+
+function PreviousSlide() {
+  currentSlide.classList.remove("current-slide");
+  if (currentIndex === 0) {
+    currentIndex = slides.length - 1;
+    currentSlide = slides[currentIndex];
+    currentSlide.classList.add("current-slide");
+    return;
+  }
+  currentIndex -= 1;
+  currentSlide = slides[currentIndex];
+  currentSlide.classList.add("current-slide");
+}
+// For Common component Hero Slides
